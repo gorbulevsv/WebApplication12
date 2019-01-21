227871f2c9af1f146a6df0eab11f99acbef89490
Горбулёв Сергей <gorbulevsv@yandex.ru>
2019-01-21 10:42:17 +0300

    Добавьте файлы проекта.

---
+++ WebApplication12/Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication12.Models
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
